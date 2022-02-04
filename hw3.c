5a49f38c5341d7c77895aecb2244759b53372e10
violetrs521 <smithvr@g.cofc.edu>
2022-02-03 20:12:01 -0500

    added the c file

---
+++ hw3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+
+    // gets the ascii of an int from the command 
+    // line of the block size of memory you 
+    // would like to allocate
+    char* a = argv[1];
+
+   
+
+    //prints the value you entered
+    printf("you have entered %s\n", a);
+
+   
+
+
+    
+
+  
+
+
+   
+
+    return 0;
+}   
