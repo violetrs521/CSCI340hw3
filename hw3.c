bf685b157dea66bad89cf4a8fcdd9fb89bea576d
violetrs521 <smithvr@g.cofc.edu>
2022-02-03 22:46:49 -0500

    Working project

--- hw3.c
+++ hw3.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+
 
 
 int main(int argc, char *argv[]){
 
-    // gets the ascii of an int from the command 
-    // line of the block size of memory you 
-    // would like to allocate
-    char* a = argv[1];
-
-   
-
-    //prints the value you entered
-    printf("you have entered %s\n", a);
-
-   
-
-
+    
+    
+    char *arr[argc];
+    printf("argv[1] %s\n", argv[1]);
     
 
-  
+    if(argc < 2){
+        printf("error hello\n");
 
+    }
+    else{
+        
+        for(int i = 1; i < argc; i++){
+ 
+            arr[i-1] = argv[i];
+            //printf("This is the arr %s\n", arr[i]);
+        }
+        arr[argc-1] = NULL;
+        //printf("This should be null: %s\n", arr[argc-1]);
 
-   
+        pid_t cpid;
+        if(fork() == 0){
+        
+            printf("I am baby\n");
+            execvp(argv[1], arr);
+            exit(0);
+         
+        }
+        else{
+            printf("I am not baby\n");
+            cpid = wait(NULL);
+        }
+      
+    }
+    
 
     return 0;
 }   
